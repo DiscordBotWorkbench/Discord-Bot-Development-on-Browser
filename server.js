1931e8493425a25a98fdedaa9c81e71935f773a0
Glitch (hello-express) <none>
2020-08-02 16:20:45 +0000

    😹✈️ Checkpoint ./hey:20306658/139 ./server.js:20306658/2200 ./package.json:20306658/1513

--- server.js
+++ server.js
@@ -1,34 +1,18 @@
-// server.js
-// where your node app starts
+//Clear everything in this file! I am uploading the code aswell on Github! Delete aswell the other things like .env, public, views
+//Yet we add the package that we need!
+//Lets start coding!
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const Discord = require('discord.js');//We say that we need this module!
+const client = new Discord.Client();//We creating a new Client!
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+client.once('ready', () => {//We are testing if the client is ready, when he is ready the code inside the {} is starting!
+          console.log("I'm ready...")//With this Command we can see in Console if he is ready! You can customize the text inside the ""
+          client.user.setActivity("I'm in development!")//This is the status of the Bot! You can customize the status inside the "", maybe i will show you later another method!
+          });
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+//Let us start with an easy Command
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+  client.on("message", message => {
+    if()
+  })
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
