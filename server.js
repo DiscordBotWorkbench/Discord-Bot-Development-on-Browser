19675516dfab49501028d2d0d9e8349ebf59745b
Glitch (hello-express) <none>
2020-08-02 16:31:48 +0000

    🚇💵 Checkpoint ./server.js:20306658/1333

--- server.js
+++ server.js
@@ -12,7 +12,22 @@ client.once('ready', () => {//We are testing if the client is ready, when he is 
 
 //Let us start with an easy Command
 
-  client.on("message", message => {
-    if()
+  client.on("message", message => {//This will start when a the bot recieve an Message!
+    if(message.content === "!ping"){//This will start if the message is !ping
+      return message.channel.send(`:ping_pong: Your Ping is ${client.ws.ping}`)//This will return the message its written inside the `` these do the same as "" but you can easylier add variables... This in an variable
+    }
   })
+
+client.login("NzM5NTE5Njk4NDcwOTYxMjc4.XybpYw.MIGE8MRLcmcWjRGoVeL9bJ7ln4c")//This login the Token! I will show you how to become an Token! first go to https://discord.com/developers/applications! I will put the link in desciption! Please follow me!
+
+
+//So, yet we have an Token! But please be careful with your token! When anybody had your token! He can change the code! I will reset my token after recording!
+
+
+
+
+
+// Lets click on Tools and Logs to check if he is ready
+
+//Perfect ! Yet we can invite the bot to server! Follow me to https://discord.com/developers/applications again!
 
